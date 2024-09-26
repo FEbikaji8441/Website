abf9babf9fc0b2c8390a3aabf83d0682092cfa37
FEbikaji8441 <febikajicvn11b@gmail.com>
2024-09-27 03:28:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    //port: process.env.DB_PORT || 3306 // Use the "Jawan" database
+    port: process.env.DB_PORT || 3306 // Use the "Jawan" database
 });
 
 // Connect to the database
